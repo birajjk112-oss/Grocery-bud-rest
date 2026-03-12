3b62605b3e55dca3981b3865dad892ad36abe799
birajjk112 <birajjk112@gmail.com>
2026-03-12 13:27:55 +0545

    New

--- grocery_project/settings.py
+++ grocery_project/settings.py
@@ -151,8 +151,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://grocery-bud-rest.vercel.app",
-    "https://rest-apigrocery.netlify.app",
+    "https://grocery-bud-rest-rvpv.vercel.app",
+    "https://courageous-faun-2ca78a.netlify.app/",
     "https://*.vercel.app",
 ]
 
